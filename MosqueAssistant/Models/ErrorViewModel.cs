e28d7fd1bdb77094dbd8a6e973f0a1b529d85f82
SlashtimeSolutions <34519458+SlashtimeSolutions@users.noreply.github.com>
2023-06-04 13:32:48 -0400

    20230604 Start

---
+++ MosqueAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MosqueAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
